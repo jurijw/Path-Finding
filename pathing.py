8711745cb56d54df4f09dea03f5fa195063b3476
= <=>
2020-01-19 01:56:08 +0100

    Added function to update nearest neighbors.

--- pathing.py
+++ pathing.py
@@ -7,20 +7,15 @@ size = width, height = (800, 800)
 cell_count = cells_horizontal, cells_vertical = (50, 50)
 cell_size = cell_width, cell_height = (width // cells_horizontal, height // cells_vertical)
 
+# Colors
 white = (255, 255, 255)
 black = (0, 0, 0)
+red = (255, 0, 0)
+dark_green = (0, 100, 0)
+light_green = (100, 255, 100)
+blue = (0, 0, 255)
+purple = (255, 0, 255)
 
-class Square:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = x
-        self.obstacle = False
-
-    def __repr__(self):
-        return f"Square({self.x}, {self.y})"
-
-    def __str__(self):
-        return f"Square() x: {self.x}, y: {self.y}, obstacle: {self.obstacle}"
 
 
 def setup_grid():
@@ -39,8 +34,14 @@ def display_grid(screen, grid):
     # Loop through all elements in the grid
     for j, row in enumerate(grid):
         for i, square in enumerate(row):
-            # Set the square color
-            square_color = black if square.obstacle else white
+            # Set the square color - defaults to white
+            square_color = white 
+            if square.obstacle:
+                square_color = black
+            elif square.start:
+                square_color = light_green
+            elif square.finish:
+                square_color = dark_green
 
             # Draw the square in the appropriate color
             pygame.draw.rect(screen, square_color, (i * cell_width, j * cell_height, cell_width, cell_height), 0)
@@ -80,6 +81,68 @@ def mouse_to_coord():
 
     return (x_coord, y_coord)
 
+def set_obstacles(grid):
+    # Get mouse position
+    x, y = mouse_to_coord()
+
+    # Set the selected squares to be obstacles
+    grid[y][x].obstacle = True
+
+
+class Square:
+    # 2d list to keep track of visited squares /  nodes
+    visited = [[False for _ in range(cells_horizontal)] for _ in range(cells_vertical)]
+
+    def __init__(self, x, y):
+        # location
+        self.x = x
+        self.y = x
+        # type
+        self.obstacle = False
+        self.start = False
+        self.finish = False
+        # node information
+        self.current = False
+        self.visited = False
+        self.distance = float("inf")
+    
+
+    def __repr__(self):
+        return f"Square({self.x}, {self.y})"
+
+    def __str__(self):
+        return f"Square() x: {self.x}, y: {self.y}, obstacle: {self.obstacle}"
+
+
+
+def nearest_neighbor_distance(grid, pos):
+    """
+    Takes the pos of a node and updates the distance 
+    attribute on all of its unchecked neighboring nodes
+    that are not obstacles.
+    """
+    x, y = pos
+    current_node = grid[y][x]
+    current_distance = current_node.distance
+    # vectors from any given node to all its possible neighbors 
+    vectors_to_neighbors = ((1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1))
+    # Calculate nearest neighbor distance for each neighbor
+    for vector in vectors_to_neighbors:
+        dx, dy = vector
+        new_x, new_y = x + dx, y + dy
+        # Check if neighbor exists on the grid
+        if new_x >= 0 and new_x < cells_horizontal and new_y >= 0 and new_y < cells_vertical:
+            neighbor = grid[new_y][new_x]
+            # Unless it is an obstacle or has been visited, update its distance attribute
+            if not neighbor.obstacle and not neighbor.visited:
+                neighbor.distance = min(neighbor.distance, current_distance + 1)
+    
+    # Count the current node as visited and update Square.visited to reflect this
+    current_node.visited = True
+    Square.visited[y][x] = True
+
+def dijkstra(start_node):
+
 
 def main():
 
@@ -89,14 +152,40 @@ def main():
 
     # Setup the grid
     grid = setup_grid()
-
+    
     # Initialize variables
-    drawing_mode = False
     end_program = False
+    drawing_mode = False
+    insert_modes = ("obstacle", "start", "stop", "erase")
+    mode = 0 
+
+
+
+    # FIX - added temporary start and finish fields
+    start_pos = start_x, start_y = (3, 4)
+    finish_pos = finish_x, finish_y = (35, 42)
+
+    start = grid[start_y][start_x]
+    finish = grid[finish_y][finish_x]
+    # set start and finish node settings
+    start.start = True
+    finish.finish = True 
+
+    start.distance = 0
+    start.current = True
+
+
+
+
     while not end_program:
         for event in pygame.event.get():
+            # Check for quit
             if event.type == pygame.QUIT:
                 end_program = True
+
+        # Check for a toggle of insert mode
+        #print(type(pygame.K_LCTRL)) # returns int: 306
+        # print(pygame.key)   
        
         # Display the grid
         display_grid(screen, grid)
@@ -105,13 +194,10 @@ def main():
         if check_drawing_mode_toggle():
             drawing_mode = not drawing_mode            
 
-        # If drawing mode is enabled get the mouse position
-        if drawing_mode:
-            x, y = mouse_to_coord()
-
-            # Set the selected squares to be obstacles
-            grid[y][x].obstacle = True
-
+        # If drawing mode is enabled set the obstacles
+        if drawing_mode and insert_modes[mode] == "obstacle":
+            set_obstacles(grid)
+        
     # End program
     pygame.quit()
     sys.exit(0)
